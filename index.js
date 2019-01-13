2afef578d25cf327d7b1c46d9a7e7f81b0d520b7
github username <github email address>
2019-01-12 20:23:32 -0600

    Done.

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ function getRepositories(){
 
 function showRepositories(event, data){
     // what's going into event/data?
-    console.log(this.responseText)
-    let repoList = "<ul>"
-    for (var i = 0; i < this.responseText.length; i++){
-        repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-    }
-    repoList += "</ul>"
-    document.getElementById("repositories").innerHTML = repoList
+    var repos = JSON.parse(this.responseText);
+    console.log(repos);
+    const repoList = `<ul>${repos
+      .map(r => "<li>" + r.name + "</li>")
+      .join("")}</ul>`;
+    document.getElementById("repositories").innerHTML = repoList;
 }
 
-showRepositories()
